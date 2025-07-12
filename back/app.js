d2e4fcb1b7bb0436d78ea32c1ede53b4b46f5440
Kostiantyn Kolomiichuk <kkolomiychuk@cleack.com>
2025-07-12 16:07:19 +0300

    prepare for openAPI

--- back/app.js
+++ back/app.js
@@ -2,10 +2,18 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import errorsCatcher from "./middlewares/errorsCatcher.js";
 import apiRouter from "./routes/apiRouter.js";
 
+
 import "./db/sync.js";
+
+
 
 const app = express();
 
@@ -13,7 +21,14 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const openapiDocument = YAML.load(path.join(__dirname, 'docs', 'openapi.yaml'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiDocument));
+
+
 app.use(express.static("public"));
+app.use("/api/users", followsRouter);
 
 app.use("/api", apiRouter);
 
