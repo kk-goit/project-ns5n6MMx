e6c7b580cdfef467da291cd7f1c801134213162b
Kostiantyn Kolomiichuk <kkolomiychuk@cleack.com>
2025-07-14 05:30:46 +0300

    WIP: Added the Swagger openAPI

--- back/app.js
+++ back/app.js
@@ -2,18 +2,11 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 import errorsCatcher from "./middlewares/errorsCatcher.js";
 import apiRouter from "./routes/apiRouter.js";
-
+import swaggerRouter from "./routes/swaggerRouter.js";
 
 import "./db/sync.js";
-
-
 
 const app = express();
 
@@ -21,22 +14,16 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const openapiDocument = YAML.load(path.join(__dirname, 'docs', 'openapi.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiDocument));
-
-
 app.use(express.static("public"));
-app.use("/api/users", followsRouter);
 
 app.use("/api", apiRouter);
+app.use('/api-docs', swaggerRouter);
+
+app.use(errorsCatcher);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
-
-app.use(errorsCatcher);
 
 const { PORT = 3000 } = process.env;
 const port = Number(PORT);
