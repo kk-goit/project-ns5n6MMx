776fb2b8464a3563235ddecffcba593610c2284a
Kostiantyn Kolomiichuk <kkolomiychuk@cleack.com>
2025-07-21 11:59:33 +0300

    Reworked registration - added auto login

--- back/services/userService.js
+++ back/services/userService.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import { generateAvatar } from "../services/avatarService.js";
+import authServices from "./authServices.js";
 import { User } from "../db/models/users.js";
 import ConflictError from "../errors/conflictError.js";
 import { UniqueConstraintError } from "sequelize";
@@ -13,11 +14,25 @@ export const registration = async (body) => {
             password,
             avatar,
         });
+
+        if (!user) { 
+            throw new HttpError(409, "Registration failed");
+        }
+        const token = authServices.generateToken(user.id);
+        const result = await authServices.updateUserToken(user.id, token);
+
+        if (!result) {
+            await user.destroy();
+            throw new HttpError(409, "Registration failed");
+        }
         return {
-            id: user.id,
-            email: user.email,
-            name: user.name,
-            avatar: user.avatar,
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                avatar: user.avatar,
+            }
         };
     } catch (error) {
         if (error instanceof UniqueConstraintError) {
